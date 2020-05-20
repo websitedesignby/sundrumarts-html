4243e16730b4a2008dd9bee0b6f86a8d02efe706
websitedesignby <ross@webdesignby.com>
2020-05-20 14:09:46 -0400

    initial commit

---
+++ html/js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+
+  $('.mobile-trigger').click( function() {
+    $(this).children('.mobile-nav').toggleClass('open');
+  });
+
+
+
+    console.log( "1-2-3-GO!" );
+});
---
+++ html/template/footer.php
@@ -0,0 +1,21 @@
+<footer class="md-pb-3 md-pt-3 pt-6 pb-6">
+<div class="wrapper txt-center">
+<p>Minneapolis, MN USA<br />
+david@sundrumarts.com<br />
+www.sundrumarts.com<br />
+(612) 385-5707<br />
+<a class="txt-black" href="/" target="_blank">LinkedIn</a></p>
+</div>
+</footer>
+
+<div class="footer-extend bg-black pt-2 pb-2">
+<div class="wrapper">
+<div class="row">
+<div class="col-md-12 col-xl md-txt-center txt-leftt"><img alt="Footer Logo" class="footer-logo" src="img/dave-sundrum-arts-white-logo.svg" /></div>
+
+<div class="col-md-12 col-xl md-txt-center txt-right">
+<p>Copyright &copy; 2019 Sundrum Arts</p>
+</div>
+</div>
+</div>
+</div>
